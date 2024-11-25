a8e5391f6a937569a88d7993d64e039f5151b62f
lecroissant795 <st22424@ormiston.school.nz>
2024-11-25 14:31:53 +1300

    Header done

---
+++ front-end/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Nunito, Arvo } from 'next/font/google';
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const nunito = Nunito({ subsets: ['latin'], variable: '--font-sans'});
+const arvo = Arvo({subsets: ['latin'], variable: '--font-serif', weight: ['400']});
+
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "My Portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={twMerge(
+        nunito.variable, 
+        arvo.variable,
+        ("bg-gray-900 text-white font-sans")
+        )}>{children}</body>
+    </html>
+  );
+}
