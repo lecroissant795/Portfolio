a0c063a1fe1634c9736532bb185f06048ec9be5d
lecroissant795 <st22424@ormiston.school.nz>
2024-11-28 03:53:47 +1300

    Features Section Update

--- front-end/src/sections/Projects.tsx
+++ front-end/src/sections/Projects.tsx
@@ -35,7 +35,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16">
+    <section className="pb-16 lg:py-24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-purple-300 to-sky-400 text-transparent bg-clip-text">
@@ -45,7 +45,7 @@ export const ProjectsSection = () => {
         <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
           Featured Projects
         </h2>
-        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
           See how I transformed concepts into engaging digital experiences.
         </p>
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
@@ -63,23 +63,23 @@ export const ProjectsSection = () => {
                   <span>&bull;</span>
                   <span>{project.year}</span>
                 </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4"/>
-              <ul className="flex flex-col gap-4 mt-4">
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5"/>
+              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                 {project.results.map((result) => (
-                  <li className="flex gap-2 text-sm text-white/50">
-                    <CheckCircleIcon className="size-5"/>
+                  <li className="flex gap-2 text-sm md:text-base text-white/50">
+                    <CheckCircleIcon className="size-5 md:size-6"/>
                     <span>{result.title}</span>
                   </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
                   <span>View Site</span>
                   <ArrowUpRightIcon className="size-4"/>
                   </button>
               </a>
-              <Image src={project.image} alt={project.title} className="mt-8 -mb-4" />
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0" />
             </div>
           ))}
         </div>
