8c353c08f52614b15ff1e4a4a10aa97e8e507d19
lecroissant795 <st22424@ormiston.school.nz>
2024-11-30 16:12:40 +1300

    Tape Section Completed

--- front-end/src/sections/Tape.tsx
+++ front-end/src/sections/Tape.tsx
@@ -1,3 +1,32 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Maintainable",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16">
+      <div className="bg-gradient-to-r from-purple-600 to-fuchsia-600 overflow-x-clip -rotate-3 -mx-1">
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div key={word} className="inline-flex gap-4 items-center">
+                <span className="uppercase font-extrabold text-sm">{word}</span>
+                <StarIcon className="size-6 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
